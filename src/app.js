25196cf8202daf0a2ce17ba77f304ea792efbaf9
Kimberly Cable <cableredi@comcast.net>
2020-04-07 13:03:00 -0600

    added db, knex, logging

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
-require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const helmet = require('helmet');
 const { NODE_ENV } = require('./config');
+const errorHandler = require('./error-handler');
 
 const app = express();
 
@@ -16,19 +16,10 @@ app.use(helmet());
 app.use(cors());
 
 app.get('/', (req, res) => {
-  res.send('Hello, world!')
+  res.json({ok: true});
 });
 
 /* Error handling */
-app.use(function errorHandler(error, req, res, next) {
-  let response;
-  if (NODE_ENV === 'production') {
-    response = { error: { message: 'server error' } };
-  } else {
-    console.error(error);
-    response = { message: error.message, error };
-  }
-  res.status(500).json(response);
-})
+app.use(errorHandler);
 
 module.exports = app;
